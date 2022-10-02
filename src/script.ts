37bf2ccc92f2600a812372d76086fb9ee9b32eaa
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-10-02 21:21:49 +0600

    Explicit A union Types - Typescript

---
+++ src/script.ts
@@ -0,0 +1,24 @@
+const country = "Bangladesh";
+console.log(country);
+
+// normal variable
+let b: string | number;
+
+// array 
+let a: (string | number) [] = [];
+
+b = 'Emon';
+a.push('Emon', 21);
+
+// objects
+let c: {
+    name: string,
+    age: number,
+    adult: boolean
+}
+
+c =  {
+    name:  "emon",
+    age: 21,
+    adult: true,
+}
