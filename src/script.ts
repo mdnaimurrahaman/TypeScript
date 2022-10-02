18eb7eafea61337c985959d896f08f81d802c4c7
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-10-02 21:33:41 +0600

    Dynamic type - Typescript

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,7 @@
 const country = "Bangladesh";
 console.log(country);
 
+/* 
 // normal variable
 let b: string | number;
 
@@ -21,4 +22,26 @@ c =  {
     name:  "emon",
     age: 21,
     adult: true,
+}
+*/
+
+// 05 Dynamic Type OR any Type.
+//variable
+let a: any;
+
+a = 5;
+a =  "Emon";
+
+//Array
+let array: any[] = [];
+array.push(21, 'emon');
+
+//objects
+let b: {
+    name: any,
+    age: any
+}
+b = {
+    name: 'Bangladesh',
+    age:  21
 }
